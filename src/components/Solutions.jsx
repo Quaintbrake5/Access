3e11b625e520715f6d5a43ddc0d932f53eb0ae66
Quaintbrake5 <denzylibe6@gmail.com>
2024-06-19 13:40:17 +0100

    Added my project

--- src/components/Solutions.jsx
+++ src/components/Solutions.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import '../components/styles/solution.css'
 
 const Solutions = () => {
   return (
@@ -20,9 +21,41 @@ const Solutions = () => {
             </div>
 
             <div className='solution-banking'>
-                <div className='solution-card'>
-                    <img src='../image/handshake.png'></img>
+                <div className='solution-card-0'>
+                    <div className='blue'>
+                        <h3 className="finance">Financing</h3>
+                        <div className="bluetext">
+                            <p>Based on specific requirements to bridge funding gaps with flexible repayment structures.</p>
+                        </div>
+                        <a href='https://www.accessbankplc.com/contact-us/agency-banking'>access more</a>
+                    </div>
+
                 </div>
+
+                <div className='solution-card-1'>
+                    <div className='blue'>
+                        <h3 className="finance">Equipping Growing Businesses</h3>
+                        <div className="bluetext">
+                            <p>We believe in growth and sustainability and we want to grow your business with you. See how we can help.</p>
+                        </div>
+                        <a href='https://www.accessbankplc.com/contact-us/agency-banking'>access more</a>
+                    </div>
+
+                </div>
+
+                <div className='solution-card-2'>
+                    <div className='blue'>
+                        <h3 className="finance">Sustainable Banking</h3>
+                        <div className="bluetext">
+                            <p>For over two decades, we have been taking actionable steps towards sustainability in a rapidly changing world.</p>
+                        </div>
+                        <a href='https://www.accessbankplc.com/contact-us/agency-banking'>access more</a>
+                    </div>
+
+                </div>
+
+                <br/>
+
             </div>
         </div>
       </section>
