1a1d6ee5b7a07d52092a29e56f5cdf6d345b9227
Quaintbrake5 <denzylibe6@gmail.com>
2024-06-20 22:12:59 +0100

    Added my project

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Promotion from './components/Promotion'
 import Solutions from './components/Solutions'
 import ColorLine from './components/ColorLine'
 import Helpful from './components/Helpful'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -26,8 +27,12 @@ function App() {
       <Promotion/>
       <Solutions/>
       <br/>
+      <br/>
+      <br/>
+      <br/>
       <ColorLine/>
       <Helpful/>
+      <Footer/>
     </>
   )
 }
--- src/components/NewsStrip.jsx
+++ src/components/NewsStrip.jsx
@@ -15,23 +15,28 @@ export const NewsStrip = () => {
             <span className='bend-line'></span>
             <marquee className='strip-wrap' onMouseOver= 'this.stop();' onMouseOut= 'this.start()'>
                 <a href='https://www.accessbankplc.com/media/news/access-bank-zambia-limited-completes-acquisition-of-atlas-mara,-creates-one-of-zambia%E2%80%99s-top-five-ban' className='strip'>
-                    Access Bank Zambia Limited Completes Acquisition of Atlas Mara, Creates one of Zambia's Top Five Banks
+                  <p>Access Bank Zambia Limited Completes Acquisition of Atlas Mara, Creates one of Zambia's Top Five Banks </p>
+                  <br/>
                 </a>
 
                 <a href='https://www.accessbankplc.com/media/news/notice-of-the-35th-annual-general-meeting-(agm)-of-access-bank-plc' className='strip'>
-                    Notice of the 35th Annual General Meeting (AGM) of Access Bank PLC
+                    <p>Notice of the 35th Annual General Meeting (AGM) of Access Bank PLC</p>
+                    <br/>
                 </a>
 
                 <a href='https://www.accessbankplc.com/media/news/s-p-global-ratings-provides-aligned-opinion-on-access-bank-plc-s-sustainable-finance-framework' className='strip'>
-                    S&P Global Ratings Provides Aligned Opinion on Access Bank PLC's Sustainable Finance Framework
+                    <p>S&P Global Ratings Provides Aligned Opinion on Access Bank PLC's Sustainable Finance Framework</p>
+                    <br/>
                 </a>
 
                 <a href='https://www.accessbankplc.com/media/news/access-bank-plc-(access)-enters-into-acquisition-agreements-with-standard-chartered-bank' className='strip'>
-                    Access Bank Plc (Access) enters into acquisition agreements with Standard Chartered Bank
+                    <p>Access Bank Plc (Access) enters into acquisition agreements with Standard Chartered Bank</p>
+                    <br/>
                 </a>
 
                 <a href='https://www.accessbankplc.com/media/news/access-bank-launches-the-first-american-express%C2%AE-cards-to-be-issued-in-nigeria' className='strip'>
-                    Access Bank launches the first American Express Cards to be issued to in Nigeria
+                    <p>Access Bank launches the first American Express Cards to be issued to in Nigeria</p>
+                    <br/>
                 </a>
             </marquee>
         </div>
